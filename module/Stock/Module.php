c758d5a58cce249b3f603750bd9ad445241cc5b8
Maykon R. Meier <maykon.meier@gmail.com>
2013-10-09 18:33:05 -0300

    Added threat dispatch and errors on module cfg

--- module/Stock/Module.php
+++ module/Stock/Module.php
@@ -18,7 +18,7 @@ use Zend\View\Model\JsonModel;
 use Zend\Mvc\ModuleRouteListener;
 use Zend\Mvc\MvcEvent;
 use Zend\Http\Request as HttpRequest;
-//use Zend\Feed\PubSubHubbub\HttpResponse; 
+//use Zend\Feed\PubSubHubbub\HttpResponse;
 
 class Module
 {
@@ -106,35 +106,47 @@ class Module
         $moduleRouteListener->attach($eventManager);
 
         /**
-         * @var \Zend\ModuleManager\ModuleManager $moduleManager 
+         * @var \Zend\ModuleManager\ModuleManager $moduleManager
          */
         $moduleManager = $e->getApplication()->getServiceManager()->get('modulemanager');
         /**
-         * @var \Zend\EventManager\SharedEventManager $sharedEvents 
+         * @var \Zend\EventManager\SharedEventManager $sharedEvents
          */
         $sharedEvents = $moduleManager->getEventManager()->getSharedManager();
-        $sharedEvents->attach('Zend\Mvc\Application', 'dispatch.error', array($this, 'moduleError'), 100);
+        $sharedEvents->attach('Zend\Mvc\Application', 'dispatch', array($this, 'threatDispatch'), 99);
+        $sharedEvents->attach('Zend\Mvc\Application', 'dispatch.error', array($this, 'threadDispatchError'), 100);
+    }
+
+    public function threatDispatch(MvcEvent $event) {
+        if ($event->isError()) {
+            $jsonModel = new JsonModel(array(
+                'success' => false,
+                'teste' => $event->getError(),
+            ));
+            $event->setViewModel($jsonModel);
+            $event->stopPropagation();
+            return $jsonModel;
+        } else return;
     }
 
     /**
      * Trata as excessões
      * @return json_encode
      */
-    public function moduleError($event){ 
-        //var_dump($event->getParam('exception') instanceOf \Exception); 
-        //create a JsonModel api json
-        $model = new JsonModel(array( 
-            'success'      => FALSE, 
-            'errorMessage' => $event->getError(),
+    public function threadDispatchError(MvcEvent $event){
+        $model = new JsonModel(array(
+            'header' => array(
+                'success'      => false,
+                'errorMessage' => $event->getError(),
+            ),
         ));
 
 
-        if($event->isError() && $event->getError() == 'error-exception'){ 
+        if ($event->isError() && $event->getError() == 'error-exception') {
             $model->exception = $event->getParam('exception')->getMessage();
             $model->code      = $event->getParam('exception')->getCode();
             $model->file      = $event->getParam('exception')->getFile() ;
-        }
-        elseif($event->isError() && $event->getError() == 'error-router-no-match'){
+        } elseif ($event->isError() && $event->getError() == 'error-router-no-match') {
             $response  = $event->getResponse();
             $exception = $event->getResult();
 
@@ -143,13 +155,10 @@ class Module
             $model->httpStatus = $event->getResponse()->getStatusCode();
             $model->title      = $event->getResponse()->getReasonPhrase();
         }
-        else{
-            $model->errorMessageDesconhecido = $event->getError();
-        }
 
-        $event->setViewModel($model); 
+        $event->setViewModel($model);
         $event->stopPropagation();
 
-        return $model; 
-    } 
+        return $model;
+    }
 }
