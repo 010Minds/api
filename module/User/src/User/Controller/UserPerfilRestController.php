cddfa9caa846cbfb341ea403af908301344df679
Alexsandro Andre <andre@010minds.com>
2013-10-16 11:27:15 -0300

    Metodo getList retornando meu perfil, respeitando a ordem da permissão true/false caso seja false testa o public_profile para retornas os dados

--- module/User/src/User/Controller/UserPerfilRestController.php
+++ module/User/src/User/Controller/UserPerfilRestController.php
@@ -28,18 +28,22 @@ class UserPerfilRestController extends AbstractRestfulController{
      * @return array json_encode
      */
 	public function getList(){ 
-		#code
+		$userId          = $this->params()->fromRoute('uid', false);
+		
+		return new JsonModel(array(
+            'data' => $this->getMyProfile($userId),
+        ));
 	}
-
+	
 	public function get($id){
-		$requestParams   = $this->params()->fromRoute(); 
-		//$userid          = $requestParams['uid'];
+		$requestParams   = $this->getRequest()->getUri();
+		$userUri         = $this->getPartsUri($requestParams,'user');
 		$data            = array();
 		$followingsArray = array();
 
 		//get user
 		$results = $this->getUserTable()->getUser($id);
-		
+
 		$results->id      = (int) $results->id; 
 		$results->reais   = (float) $results->reais;
 		$results->dollars = (float) $results->dollars; 
@@ -48,31 +52,109 @@ class UserPerfilRestController extends AbstractRestfulController{
 		$followers      = $this->getFollowsTable()->fullFollowers($id);
 		//get total followings
 		$followings     = $this->getFollowsTable()->fullFollowing($id);
+
+		$data = array(
+			'user'       => $results,
+			'follower'  => $followers,
+			'following' => $followings,
+		);
+
+		if(!empty($userUri) && $userUri == 'user'){
+			//get list followings
+			$dataFollowings = $this->getFollowsTable()->getFollowing($id);
+			$i = 0;
+			foreach ($dataFollowings as $value) {
+				if($i <= 9){
+					$value->id        = (int) $value->id;
+					$value->user_id   = (int) $value->user_id;
+					$value->following = (int) $value->following;
+					//mudar para retornar o user
+					$followingsArray += array('user_'.$value->following => $value);
+					$i++;	
+				}
+			}
+
+			$data['followings'] = $followingsArray;
+		}
+
+		return new JsonModel(array(
+            'data' => $data,
+        ));
+	}
+
+	/**
+     * O método complemento do getList faz retorna o perfil do user contendo as informações como:
+     * follower,following e lista de followings
+     * @return array json_encode
+     */
+	public function getMyProfile($id){
+		$data            = array();
+		$followingsArray = array();
+		
+		//get user
+		$results = $this->getUserTable()->getUser($id);
+		
+		//retirando o campo password não é necessario retorna-lo
+		unset($results->password);
+
+		$results->id             = (int) $results->id; 
+		$results->reais          = (float) $results->reais;
+		$results->dollars        = (float) $results->dollars; 
+		$results->public_profile = (boolean) $results->public_profile; 
+		
+		//get total followers
+		$followers      = $this->getFollowsTable()->fullFollowers($id);
+		//get total followings
+		$followings     = $this->getFollowsTable()->fullFollowing($id);
+
 		//get list followings
 		$dataFollowings = $this->getFollowsTable()->getFollowing($id);
-
 		$i = 0;
 		foreach ($dataFollowings as $value) {
-			if($i <= 9){
-				$value->id        = (int) $value->id;
-				$value->user_id   = (int) $value->user_id;
-				$value->following = (int) $value->following;
+			//retirando alguns dados desnecessários
+			unset($value->id);
+			unset($value->user_id);
+			unset($value->created_date);
+			unset($value->user);
 
-				$followingsArray[] = array('user' => $value);
+			if($i <= 9){
+				$value->following = (int) $value->following;
+				$resultUser = $this->getUserTable()->getUser($value->following);
+				//retirando o campo password não é necessario retorna-lo
+				unset($resultUser->password);
+
+				$resultUser->id             = (int) $resultUser->id; 
+				$resultUser->public_profile = (boolean) $resultUser->public_profile;
+
+				//teste se o following aceitou a amizade
+				if($value->permission == true){
+					$resultUser->reais          = (float) $resultUser->reais;
+					$resultUser->dollars 	  	= (float) $resultUser->dollars;
+				}else{
+					//teste se o perfil é publico/privado
+					if($resultUser->public_profile == false){
+						//retirando alguns dados desnecessários
+						unset($resultUser->dollars);
+						unset($resultUser->reais);
+						unset($resultUser->mail);
+						unset($resultUser->user);
+					}
+				}
+				$resultUser->permission     = (boolean) $value->permission;
+				
+				$followingsArray += array('user_'.$value->following => $resultUser);
 				$i++;	
 			}
 		}
 
 		$data = array(
 			'user'       => $results,
-			'follower'  => $followers,
-			'following' => $followings,
+			'follower'   => $followers,
+			'following'  => $followings,
 			'followings' => $followingsArray,
 		);
 
-		return new JsonModel(array(
-            'data' => $data,
-        ));
+		return $data;
 	}
 
 	/**
@@ -97,5 +179,21 @@ class UserPerfilRestController extends AbstractRestfulController{
 			$this->followsTable = $sm->get('Follows\Model\FollowsTable');
 		}
 		return $this->followsTable;
+	}
+	/**
+	 * Sua função é verificar se existe a palavra chave na uri e retorna-la
+	 * @param String $requestUri uri acessada pelo usuário
+	 * @param String $chave palavra que desejada a ser encontrada na uri
+	 * @return String $chave 
+	 */
+	public function getPartsUri($requestUri,$chave){
+		
+		if(empty($requestUri) || empty($chave)){
+			return false;
+		}
+		
+		$vetor    = explode('/',$requestUri);
+		$arrayKey = array_search($chave, $vetor);
+		return $vetor[$arrayKey];
 	}
 }
